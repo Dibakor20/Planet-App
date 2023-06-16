c7797e9761010948fce76e2e6bae11012e60e189
Debakor Acharjee <debakor10@gmail.com>
2023-06-17 00:30:33 +0600

    home screen update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,30 +5,33 @@ import PlanetHeader from "../components/PlanetHeader";
 import { colors } from "../themes/colors";
 import { PLANET_LIST } from "../data/PlanetList";
 import { spacing } from "../themes/spacing";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function Home() {
-    return (
-      <>
-    <View style={styles.container}>
-      <PlanetHeader />
-      <FlatList
-        contentContainerStyle={styles.list}
-        data={PLANET_LIST}
-        keyExtractor={(item) => item.name}
-        renderItem={({ item, index }) => {
-          return (
-            <View style={styles.item}>
-              <View
-                style={[styles.circle, { backgroundColor: item.color }]}
-              ></View>
-              <Text preset="h4"> {item.name}</Text>
-            </View>
-          );
-        }}
-                    itemSeparatorComponent={() => <View style={ styles.separators} />}
-      />
-     </View>
-     </>
+  return (
+    <>
+      <View style={styles.container}>
+        <PlanetHeader />
+        <FlatList
+          contentContainerStyle={styles.list}
+          data={PLANET_LIST}
+          keyExtractor={(item) => item.name}
+          renderItem={({ item, index }) => {
+            return (
+              <View style={styles.item}>
+                <View style={{flexDirection:'row'}}>
+                  <View
+                    style={[styles.circle, { backgroundColor: item.color }]}
+                  ></View>
+                  <Text preset="h4" style={styles.planetContent}> {item.name}</Text>
+                </View>
+                <AntDesign name="rightcircleo" size={24} color="white" />
+              </View>
+            );
+          }}
+        />
+      </View>
+    </>
   );
 }
 
@@ -43,16 +46,18 @@ const styles = StyleSheet.create({
   item: {
     flexDirection: "row",
     alignItems: "center",
-      padding: spacing[4],
-      borderBottomColor: colors.grey,
-      borderWidth:0.2,
+    justifyContent: "space-between",
+    padding: spacing[4],
+    borderBottomColor: colors.grey,
+    borderWidth: 0.2,
   },
   circle: {
     width: 30,
     height: 30,
     borderRadius: 30,
   },
-  separators: {
-     
+  planetContent: {
+    marginTop: spacing[2],
+    marginHorizontal:spacing[2],
   },
 });
